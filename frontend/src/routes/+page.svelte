07cbf5e5df305068486eb60ed523ce3610f8f7b4
huncholane <huncho@ghostsystems.io>
2023-03-28 10:52:13 -0700

    social tags look sick

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,7 +5,43 @@
   <title>Hygo</title>
   <meta
     name="description"
-    content="A social media experience that builds giga playlists with friends."
+    content="A social media experience that builds giga playlists with friends"
   />
-  <meta name="theme-color" content="#6C2561" />
+  <meta name="theme-color" content="#2A0F26" />
+
+  <!-- HTML Meta Tags -->
+  <title>Hygo</title>
+  <meta
+    name="description"
+    content="A social media experience that builds giga playlists with friends"
+  />
+
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://hygo.online" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Hygo" />
+  <meta
+    property="og:description"
+    content="A social media experience that builds giga playlists with friends"
+  />
+  <meta
+    property="og:image"
+    content="https://hygo.online/img/brand/the-one.png"
+  />
+
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="hygo.online" />
+  <meta property="twitter:url" content="https://hygo.online" />
+  <meta name="twitter:title" content="Hygo" />
+  <meta
+    name="twitter:description"
+    content="A social media experience that builds giga playlists with friends"
+  />
+  <meta
+    name="twitter:image"
+    content="https://hygo.online/img/brand/the-one.png"
+  />
+
+  <!-- Meta Tags Generated via https://www.opengraph.xyz -->
 </svelte:head>
